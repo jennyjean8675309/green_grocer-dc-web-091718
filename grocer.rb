b4eb2aacc9d91559d3e8083fe2b2f14604e37054
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-13 19:06:49 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ def add_values_to_coupon(cart, coupons)
     coupons.each do |inc, value|
       if item == "#{inc[:item]} W/COUPON"
         pricing[:count] = cart[inc[:item]][:count] / inc[:num]
-        pricing[:price] = inc[:cost] * inc[:num]
+        pricing[:price] = inc[:cost] * pricing[:count]
         binding.pry
       end
      end
