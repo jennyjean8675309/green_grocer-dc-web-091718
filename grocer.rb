801417be2e461cb1cb031e346febaf615b4e1dfe
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-13 17:55:45 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -38,6 +38,7 @@ def add_coupon_key(cart, coupons)
     coupons.each do |inc, value|
       if item_name == inc[:item] && inc[:num] <= pricing_info[:count]
         cart_with_coupons["#{item_name} W/COUPON"] = pricing_info
+        if cart[item_name][:count]   
           binding.pry
       
       elsif item_name == inc[:item] && inc[:num] > pricing_info[:count]
